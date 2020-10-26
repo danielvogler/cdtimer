9d21be486eeb67942541e0f2f02e30fca0bd403a
danielvogler <davogler@ethz.ch>
2020-10-26 16:45:57 +0100

    added loading of timetables to announce current task

--- countdown.py
+++ countdown.py
@@ -5,6 +5,7 @@ countdown app
 
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
 
 import os
 from threading import Thread
@@ -40,24 +41,29 @@ class CountdownApp(tk.Frame):
 
         ### time display
         self.time_display = Label(self.master, text="00:00:00", font=(self.font_type, self.font_size_time_display), borderwidth=2, fg=self.font_color_time_display, bg=self.color_background)
+        ### timetable display
+        self.timetable_display = Label(self.master, text="", font=(self.font_type, self.font_size_tt_display), borderwidth=2, fg=self.font_color_tt_display, bg=self.color_background)
 
         ### start countdown button
         self.start_button = Button(self.master, text="Start", font=(self.font_type, self.font_size_button), bg=self.color_background, fg=self.font_color_text, borderwidth=0, highlightthickness=0, command=self.start)
-
         ### close window button
         self.close_button = Button(self.master, text="Close", font=(self.font_type, self.font_size_button), bg=self.color_background, fg=self.font_color_text, borderwidth=0, highlightthickness=0, command=self.master.quit)
-
         ### turn sound ON/OFF
         self.sound_button = Button(self.master, text="Sound OFF", font=(self.font_type, self.font_size_button), bg=self.color_background, fg=self.font_color_text, borderwidth=0, highlightthickness=0, command=self.sound)
 
-        ### layout
-        self.time_display.place(relx=0.5, rely=0.5, anchor=CENTER)
+        ### browse for timetable file
+        self.browse_file = Button(self.master,text = "Browse Files", bg=self.color_background, fg=self.font_color_text,command = self.browseFiles)  
+        self.browse_file.place(relx=0.5, rely=0.1, anchor=CENTER)
+
         ### layout - set time
         self.entry.place(relx=0.2, rely=0.1, anchor=CENTER)
         self.submit_button.place(relx=0.2, rely=0.2, anchor=CENTER)
         ### layout - set sound frequency
         self.entry_sound.place(relx=0.8, rely=0.1, anchor=CENTER)
         self.set_sound_button.place(relx=0.8, rely=0.2, anchor=CENTER)
+        ### layout time/timetable
+        self.time_display.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.timetable_display.place(relx=0.5, rely=0.7, anchor=CENTER)
         ### layout - sound on/off
         self.sound_button.place(relx=0.3, rely=0.9, anchor=CENTER)
         self.start_button.place(relx=0.5, rely=0.9, anchor=CENTER)
@@ -71,7 +77,7 @@ class CountdownApp(tk.Frame):
         self.hours = 0
         self.minutes = 0
         self.seconds = 0
-        
+    
         parsed_time = self.entry.get().split(":")
 
         if len(parsed_time) == 1:
@@ -91,6 +97,13 @@ class CountdownApp(tk.Frame):
         self.update_time_display()
 
         return self.set_time
+
+
+    ### browse timetable files
+    def browseFiles(self): 
+
+        self.tt_file = filedialog.askopenfilename(initialdir = "./",title = "Select a File",filetypes = (("Text files",("all files","*.*")))) 
+        self.timetable_construction()
 
 
     ### update time display
@@ -156,6 +169,14 @@ class CountdownApp(tk.Frame):
                 self.time_display.configure(text=self.update_time_display())
                 self.after(1000, self.update_time)
 
+            ### check if new timetable interval is reached
+            if self.tt_loaded:
+                if (self.set_time - self.time) in self.tt_set_time:
+                    idx = self.tt_set_time.index(self.set_time - self.time)
+                    tt_string = str(self.tt_list[idx][0]+" "+self.tt_list[idx][1]+" #"+ self.tt_list[idx][2])
+                    self.timetable_display.configure(text=tt_string, fg=self.font_color_tt_display)
+                    self.play_sound(0.2, 600)
+
 
     ### pause countdown
     def stop(self):
@@ -165,18 +186,42 @@ class CountdownApp(tk.Frame):
         self.update_time
 
 
+    ### read in timetable file
+    def timetable_construction(self):
+
+        from construct_timetable import read_timetable_file
+
+        self.tt_loaded = True
+
+        try:
+            self.tt_file
+        except NameError:
+            print("No timetable file defined")
+        else:
+            print("Reading:", self.tt_file)
+            ### construct time timetable
+            self.tt_list, self.tt_set_time = read_timetable_file(self.tt_file)
+            ### current set
+            tt_string = str(self.tt_list[0][0]+" "+self.tt_list[0][1]+" #"+ self.tt_list[0][2])
+            ### update timetable display
+            self.timetable_display.configure(text=tt_string, fg=self.font_color_tt_display)
+
+
     ### definitions
     def definitions(self):
         self.color_background = "black"
         self.color_end = "green"
         self.font_color_time_display = "red"
+        self.font_color_tt_display = "blue"
         self.font_color_text = "white"
         self.font_size_time_display = 200
+        self.font_size_tt_display = 40
         self.font_size_button = 40
         self.button_borderwidth = 5
         self.window_resolution = "2000x1400"
         self.font_type = "Helvetica"
         self.sound_frequency = 3600
+        self.tt_loaded = False
 
 
 if __name__ == "__main__":
